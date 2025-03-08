c04ee8371d4de4b4a192ff443b10aee7480c862b
Your Name <you@example.com>
2025-03-08 18:18:08 +0800

    just finish a little part

--- main.go
+++ main.go
@@ -11,5 +11,13 @@ func main() {
 	c.OnResponse(func(r *colly.Response) {
 		fmt.Print(string(r.Body))
 	})
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Print("successfully")
 
+	})
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		fmt.Printf("foundLink: %q -> %s\n", e.Text, link)
+	})
+	c.Visit("https://skills-todo-api.eliaschen.dev/Task")
 }
